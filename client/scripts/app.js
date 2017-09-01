f348d3b038f53fbf6b40779eca097abc258dad32
Hack Reactor Student <student@hackreactor.com>
2017-09-01 16:31:48 -0700

    finish init and sending

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,40 @@
 // YOUR CODE HERE:
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+
+var app = {
+  storage: '',
+  init: function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
+  },
+  send: function(message) {
+
+    $.ajax({
+      type: "POST",
+       data: message
+     });
+  },
+  fetch: function() {
+
+  },
+
+}
+
