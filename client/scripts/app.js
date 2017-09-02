8b473a76ba49abdde10d9cfba643cc31b864c69e
Hack Reactor Student <student@hackreactor.com>
2017-09-01 20:00:53 -0700

    trying

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,22 +7,26 @@
 
 
 var app = {
-  storage: '',
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
   init: function() {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-    error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
+    // $.ajax({
+    //   // This is the url you should use to communicate with the parse API server.
+    //   url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    //   type: 'POST',
+    //   data: JSON.stringify(message),
+    //   contentType: 'application/json',
+    //   success: function (data) {
+    //     console.log('chatterbox: Message sent', data);
+    //     renderMessage("Hello");
+    //   },
+    //   error: function (data) {
+    // // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    //     console.error('chatterbox: Failed to send message');
+    //   }
+    // });
+    console.log("THISS", this)
+    app.fetch();
+    app.renderRoom();
 
   },
   send: function(message) {
@@ -33,8 +37,35 @@ var app = {
      });
   },
   fetch: function() {
-
+    $.ajax({
+      type: "GET",
+      contentType: 'application/json',
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  success: function (data) {
+        console.log('chatterbox: Message sent', data);
+        app.renderMessage("MESSAGE: ", data.results[0].objectId);
+      },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+     });
   },
 
+
+  clearMessages: function() {
+   $('#chats').html('');
+  },
+
+  renderMessage: function(message) {
+   $('#chats').append("<p>" + message + "</p>");
+  },
+  renderRoom: function(room) {
+   $('#roomSelect').append("<p>" + room + "</p>");
+  },
+
+  handleSubmit: function() {
+
+  }
 }
 
